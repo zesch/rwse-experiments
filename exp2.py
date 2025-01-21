fe9162ff4b6148b0f126125c6c54cb34b5a85d70
Torsten Zesch <torsten.zesch@gmail.com>
2025-01-21 19:55:48 +0100

    Do we still need the old exps?

---
+++ exp2.py
@@ -0,0 +1,7 @@
+from transformers import pipeline
+
+pipe = pipeline("fill-mask", model="bert-base-cased")
+results = pipe("I want to buy [MASK] car", targets=["their", "there"])
+
+for result in results:
+    print(f'{result["token_str"]}: {result["score"]}')
